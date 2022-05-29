ec2e11cfc917870d985e4eabdfc42c99191b8ef5
VaNomad <sjur.hassel@gmail.com>
2022-05-29 12:10:29 +0100

    fixing responsiveness of nav and media queries

--- Js/main.js
+++ Js/main.js
@@ -2,10 +2,8 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle('showmenu');
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('showmenu');
-  navMenu.classList.toggle('active');
 });
 
 
