3c7fde9624b8ba377b7b70555ae1ea58fc4cc341
VaNomad <sjur.hassel@gmail.com>
2022-05-30 15:36:34 +0100

    mobile menu adding functions

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const socialIcons = document.querySelector(".social-icons");
+
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('showmenu');
+  socialIcons.classList.toggle('hidesocial')
+
 });
 
 
