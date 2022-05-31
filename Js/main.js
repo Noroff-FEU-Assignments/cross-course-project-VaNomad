20492b1e199b57f1ca01528fd0ae5220fa768852
VaNomad <sjur.hassel@gmail.com>
2022-05-31 16:53:56 +0100

    simplifying code and fixing responsive issues

--- Js/main.js
+++ Js/main.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const socialIcons = document.querySelector(".social-icons");
+const ctaHome = document.querySelector(".home-logo");
+
 
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('showmenu');
-  socialIcons.classList.toggle('hidesocial')
+  socialIcons.classList.toggle('hidesocial');
+  ctaHome.classList.toggle('hidesocial');
 
 });
 
