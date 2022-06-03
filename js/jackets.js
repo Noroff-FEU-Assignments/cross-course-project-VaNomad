4af9e1417e47746aec27189afb797719a3541367
VaNomad <sjur.hassel@gmail.com>
2022-06-03 15:47:01 +0100

    made product list in js

---
+++ js/jackets.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+// `<div class="box">
+// <div class="img-box">
+//   <img src="/images/photos/blake-lisk-swdM6_NFnCs-unsplash.jpg" alt="">
+// </div>
+// <div class="text">
+//   <a href="product.html" class="btn">
+//     <h3>Calderon</h3>
+//   </a>
+// </div>
+// </div>`
+
---
+++ js/productList.js
@@ -0,0 +1,65 @@
+const productArray = [
+  {
+    name: "Calderon",
+    id: 0,
+    price: 2599,
+    description: "This jacket has amazing shell properties",
+    imgSrc: "/images/photos/blake-lisk-swdM6_NFnCs-unsplash.jpg"
+  },
+  {
+    name: "Begsby",
+    id: 1,
+    price: 2599,
+    description: "This jacket has amazing shell properties",
+    imgSrc: "/images/photos/john-vicente-KoPST4OEEQA-unsplash.jpg"
+  },
+  {
+    name: "Aeiron",
+    id: 2,
+    price: 2599,
+    description: "This jacket has amazing shell properties",
+    imgSrc: "/images/photos/janko-ferlic-_A1V4vuyXWM-unsplash.jpg"
+  },
+  {
+    name: "Vandela",
+    id: 3,
+    price: 2599,
+    description: "This jacket has amazing shell properties",
+    imgSrc: "/images/photos/kal-visuals-6TIoPIpMvLc-unsplash.jpg"
+  },
+  {
+    name: "Gedola",
+    id: 4,
+    price: 2599,
+    description: "This jacket has amazing shell properties",
+    imgSrc: "/images/photos/daniel-lincoln-kTChmgdTkwE-unsplash.jpg"
+  },
+  {
+    name: "Edilaso",
+    id: 5,
+    price: 2599,
+    description: "This jacket has amazing shell properties",
+    imgSrc: "/images/photos/dmitry-kara-YVkUmYAIJ5Y-unsplash.jpg"
+  },
+  {
+    name: "Selinium",
+    id: 6,
+    price: 2599,
+    description: "This jacket has amazing shell properties",
+    imgSrc: "/images/photos/durmus-kavcioglu-QPXIVXpW-JU-unsplash.jpg"
+  },
+  {
+    name: "Dizela",
+    id: 7,
+    price: 2599,
+    description: "This jacket has amazing shell properties",
+    imgSrc: "/images/photos/erik-brolin-2_cIV9J4NT8-unsplash.jpg"
+  },
+  {
+    name: "Artemis",
+    id: 8,
+    price: 2599,
+    description: "This jacket has amazing shell properties",
+    imgSrc: "/images/photos/fil-mazzarino-7IimdEvrXH4-unsplash.jpg"
+  }
+];
