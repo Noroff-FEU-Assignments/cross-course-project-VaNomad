484d70add38f3c53f43fd07c58e319d395bfb8f2
VaNomad <sjur.hassel@gmail.com>
2022-06-04 20:31:27 +0100

    local strg to cart.html + styling

---
+++ js/cart.js
@@ -0,0 +1,20 @@
+import getCart, { saveCart } from "./utils/storage.js";
+import { renderCartCount } from "./components/menu/menu.js";
+import { productsArray } from "./constants/productList.js";
+
+const cartItems = JSON.parse(localStorage.getItem("cart"));
+const cartList = document.querySelector(".cart-list");
+const cartTotal = document.querySelector(".total");
+
+let total = 0;
+cartItems.forEach(function(cartElement){
+  cartList.innerHTML += `
+  <div class="cart-item">
+    <h4>${cartElement.name}</h4>
+    <div class="cart-card">
+      <div style="background-image: url(${cartElement.imgSrc})" class="cart-image"></div>
+      <div class="cart-price">${cartElement.price}</div>
+    </div>
+  </div>
+  `
+});
