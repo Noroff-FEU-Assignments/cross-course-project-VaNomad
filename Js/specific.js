69117949fa7ceabe259adff3290b32665db34f0b
VaNomad <sjur.hassel@gmail.com>
2022-08-25 12:11:20 +0100

    Update specific.js

--- Js/specific.js
+++ Js/specific.js
@@ -7,49 +7,49 @@ const url = "https://rainydays.sjur.io/wp-json/wc/v3/products?consumer_key=ck_86
 
 // —————————‡————————— Functions
 async function getProducts() {
-  //fetch
+  
+  const response = await fetch(url);
 
-  console.log(fetch);
-
+  console.log(response);
 }
 getProducts();
 // // —————————‡————————— Element Selections
 // const jacketBox = document.querySelector(".container");
 
 // // —————————‡————————— Element Selections
-productsArray.forEach(function (jackets) {
-  console.log(jackets);
-  jacketBox.innerHTML =
-`
-   <div class="box">
-             <div class="img-box">
-               <img src="${jackets.imgSrc}" alt="${jackets.description}">
-             </div>
-             <div class="options-box flex-column">
-               <div class="colours-box">
-                 <div class="flex-row">
-                   <h1 class="title-text">choose your jacket color</h1>
-                 </div>
-                 <div class="flex-row">
-                   <div class="swatch one"></div>
-                   <div class="swatch two"></div>
-                   <div class="swatch three"></div>
-                 </div>
-               </div>
-               <div class="sizes-box flex-column">
-                 <div class="flex-row">
-                   <h1 class="title-text">choose your jacket size</h1>
-                 </div>
-                 <div class="flex-row">
-                   <div class="size"><h2>xs</h2></div>
-                   <div class="size"><h2>s</h2></div>
-                   <div class="size"><h2>m</h2></div>
-                   <div class="size"><h2>l</h2></div>
-                   <div class="size"><h2>xl</h2></div>
-                 </div>
-               </div>
-             </div>
-           </div>
-   `
+// productsArray.forEach(function (jackets) {
+//   console.log(jackets);
+//   jacketBox.innerHTML =
+// `
+//    <div class="box">
+//              <div class="img-box">
+//                <img src="${jackets.imgSrc}" alt="${jackets.description}">
+//              </div>
+//              <div class="options-box flex-column">
+//                <div class="colours-box">
+//                  <div class="flex-row">
+//                    <h1 class="title-text">choose your jacket color</h1>
+//                  </div>
+//                  <div class="flex-row">
+//                    <div class="swatch one"></div>
+//                    <div class="swatch two"></div>
+//                    <div class="swatch three"></div>
+//                  </div>
+//                </div>
+//                <div class="sizes-box flex-column">
+//                  <div class="flex-row">
+//                    <h1 class="title-text">choose your jacket size</h1>
+//                  </div>
+//                  <div class="flex-row">
+//                    <div class="size"><h2>xs</h2></div>
+//                    <div class="size"><h2>s</h2></div>
+//                    <div class="size"><h2>m</h2></div>
+//                    <div class="size"><h2>l</h2></div>
+//                    <div class="size"><h2>xl</h2></div>
+//                  </div>
+//                </div>
+//              </div>
+//            </div>
+//    `
 
-});
+// });
