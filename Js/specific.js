8d9aa024fcd24f9a6310c8759bb44b79f80360c7
VaNomad <sjur.hassel@gmail.com>
2022-08-28 23:03:04 +0100

    last

--- Js/specific.js
+++ Js/specific.js
@@ -1,33 +1,39 @@
-const title = document.querySelector(".title");
-const productImg = document.querySelector(".productImg");
-const productDiscription = document.querySelector(".productDescription");
-
-
 const detailsContainer = document.querySelector(".details");
+const result = document.querySelector(".result");
+
+
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 console.log(params);
 
 const id = params.get("id");
+console.log(id);
 
-const url = "https://rainydays.sjur.io/wp-json/wc/store/products/" + id;
+
+const url = `https://rainydays.sjur.io/wp-json/wc/store/products/${id}`;
 console.log(url);
+
+function createHTML(result) {
+  detailsContainer.innerHTML = `
+    <a class="productName" href="${result.id}">
+        <h1>${result.name}</h1>
+        <img src="${result.images[0].src}" alt="${result.name}">
+        <h4>${result.discription}</h4>
+        `;
+}
+
 
 async function getJacket() {
   try {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
-
-    title.innerHTML = products.name;
-    productImg.src = products.images[0].src;
-    productDiscription.innerHTML = products.discription;
+    createHTML(data)
 
   } catch (error) {
     console.log(error);
     detailsContainer.innerHTML = "The API call failed";
   }
-
 }
 
 getJacket(url);
